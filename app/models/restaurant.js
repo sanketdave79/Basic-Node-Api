ec632c009b7c6f06856abab7986c351056fe12dc
sanketdave79 <sanketdave79@techmentaion.com>
2016-06-03 21:39:29 +1000

    Creating Restaurant Model

---
+++ app/models/restaurant.js
@@ -0,0 +1,9 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var RestaurantSchema   = new Schema({
+    name: String,
+    location: String
+});
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
