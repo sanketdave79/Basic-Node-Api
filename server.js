dbc6637bb7aa3d68971dc9375997e7e811543c67
sanketdave79 <sanketdave79@techmentaion.com>
2016-06-19 18:33:50 +1000

    Allow Same Origin server links to perform rest operations.

--- server.js
+++ server.js
@@ -15,6 +15,15 @@ var mongoose   = require('mongoose');
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:63342');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 
 var port = process.env.PORT || 8080;        // set our port
 var Bear     = require('./app/models/bear');
